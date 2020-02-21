ce7238bf2860f54efef0176c38eb0f29a4bdeb70
Mike Panetta <panetta.mike@gmail.com>
2020-02-20 21:37:41 -0500

    Playing around, figuring things out.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use dbus::blocking::Connection;
 use std::time::Duration;
 use dbus::{Message, Path};
 use dbus::message::MatchRule;
-use dbus::arg::{ReadAll, RefArg, Variant};
+use dbus::arg::{ReadAll, RefArg, Variant, ArgAll};
 use std::borrow::Borrow;
 
 mod networkmanager;
@@ -26,31 +26,137 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //conn.add_match_no_cb("interface='org.freedesktop.DBus.Properties'")?;
 
     //let mut matchrule = MatchRule::new_signal("org.freedesktop.DBus.Properties", "PropertiesChanged");
-    let mut matchrule = MatchRule::new_signal("org.freedesktop.DBus.ObjectManager", "InterfacesAdded");
+    let matchadded = MatchRule::new_signal("org.freedesktop.DBus.ObjectManager", "InterfacesAdded");
+    let matchremoved = MatchRule::new_signal("org.freedesktop.DBus.ObjectManager", "InterfacesRemoved");
+    let matchpropertieschanged = MatchRule::new_signal("org.freedesktop.DBus.Properties", "PropertiesChanged");
+
     //matchrule.path_is_namespace = true;
     //matchrule.path = Some(Path::new("/org/freedesktop/UDisks2")?);
 
     //conn.add_match(matchrule, |arg: udisks2::OrgFreedesktopDBusPropertiesPropertiesChanged, z, msg: &Message| {
-    conn.add_match(matchrule, |arg: udisks2::OrgFreedesktopDBusObjectManagerInterfacesAdded, z, msg: &Message| {
-        println!("I MATCH!");
+
+    fn interfaces_added_watcher(arg: udisks2::OrgFreedesktopDBusObjectManagerInterfacesAdded, _conn: &Connection, _msg: &Message) -> bool {
+        //println!("I MATCH: {:?}", arg);
+
         if let Some(blockdata) = arg.interfaces_and_properties.get("org.freedesktop.UDisks2.Block") {
             println!("BD: {:?}", blockdata);
             if let Some(size) = blockdata.get("Size") {
-                println!("SIZE: {:?}", size.as_u64().unwrap())
+                println!("\tSIZE: {:?}", size.as_u64().unwrap())
             }
             if let Some(device) = blockdata.get("PreferredDevice") {
-                let device: & Variant<Box<dyn RefArg + 'static>> = device;
+                let device: &Variant<Box<dyn RefArg + 'static>> = device;
                 let device = &device.0;
                 let device = dbus::arg::cast::<Vec<u8>>(device);
-                println!("DEVICE: {:?}", std::str::from_utf8(device.unwrap()));
+                println!("\tDEVICE: {:?}", std::str::from_utf8(device.unwrap()).unwrap());
             }
         }
+
+        let loop_data = arg.interfaces_and_properties.get("org.freedesktop.UDisks2.Loop");
+        let partition_data = arg.interfaces_and_properties.get("org.freedesktop.UDisks2.Partition");
+
+        if loop_data.is_some() {
+            println!("LD: {:?}", loop_data.unwrap());
+        };
+
+        if partition_data.is_some() {
+            println!("PD: {:?}", partition_data.unwrap());
+            let partition_data = partition_data.unwrap();
+
+            let uuid = &partition_data["UUID"];
+            let size = &partition_data["Size"].as_u64().unwrap();
+            let table = &partition_data["Table"];
+            let name = &partition_data["Name"];
+            let offset = &partition_data["Offset"].as_u64().unwrap();
+            let type_ = &partition_data["Type"].as_str().unwrap();
+            let number = &partition_data["Number"].as_u64().unwrap();
+
+            println!("\tUUID:\t\t{:?}", uuid.as_str().unwrap());
+            println!("\tNUMBER:\t\t{:?}", number);
+            println!("\tSIZE:\t\t{:?}", size);
+            println!("\tTABLE:\t\t{:?}", table.as_str().unwrap());
+            println!("\tNAME:\t\t{:?}", name.as_str().unwrap());
+            println!("\tOFFSET:\t\t{:?}", offset);
+            println!("\tTYPE:\t\t{:?}", type_);
+
+        };
+
         //println!("ARG: {:?}", arg.interfaces_and_properties);
         //println!("CON: {:?}", z.);
         //println!("MSG: {:?}", msg);
 
         true
-    });
+    }
+
+    fn interfaces_removed_watcher(arg: udisks2::OrgFreedesktopDBusObjectManagerInterfacesRemoved, _conn: &Connection, _msg: &Message) -> bool {
+        println!("I MATCH: {:?} => {:?}", arg, _msg);
+        if arg.interfaces.contains(&"org.freedesktop.UDisks2.Partition".to_owned()) {
+            //stuff
+            println!("PARTITION_REMOVED: {:?} => {:?}", arg.object_path, _msg.path())
+        }
+
+        if arg.interfaces.contains(&"org.freedesktop.UDisks2.Block".to_owned()) {
+            //stuff
+            println!("BLOCK_REMOVED: {:?}", arg.object_path);
+        }
+
+        if arg.interfaces.contains(&"org.freedesktop.UDisks2.PartitionTable".to_owned()) {
+            //stuff
+            println!("PARTITION_TABLE_REMOVED: {:?}", arg.object_path.to_string());
+        }
+
+        true
+    }
+
+    fn properties_changed_watcher(arg: udisks2::OrgFreedesktopDBusPropertiesPropertiesChanged, conn: &Connection, msg: &Message) -> bool {
+        return match arg.interface_name.as_str() {
+            "org.freedesktop.UDisks2.Block" => {
+                println!("BLOCK: {:?} => {:?}", msg.path(), arg.changed_properties);
+                true
+            },
+            "org.freedesktop.UDisks2.Loop" => {
+                if let Some(backing_file) = arg.changed_properties.get("BackingFile") {
+                    let backing_file: &Variant<Box<dyn RefArg + 'static>> = backing_file;
+                    let backing_file= dbus::arg::cast::<Vec<u8>>(&backing_file.0).unwrap();
+
+                    println!("LOOP: {:?} => {:?}", msg.path(), std::str::from_utf8(backing_file).unwrap());
+                }
+
+
+                true
+            },
+            "org.freedesktop.UDisks2.Partition" => {
+                println!("PARTITION: {:?}", arg.changed_properties);
+                true
+            },
+            "org.freedesktop.UDisks2.PartitionTable" => {
+                println!("PTABLE: {:?}", arg.changed_properties);
+                true
+            },
+            "org.freedesktop.UDisks2.Filesystem" => {
+                println!("FILESYSTEM: {:?}", arg.changed_properties);
+                true
+            },
+            "org.freedesktop.systemd1.Device" => {
+                println!("DEVICE: {:?} => {:?}", msg.path(), arg.changed_properties);
+                true
+            },
+            "org.freedesktop.systemd1.Unit" => {
+                println!("UNIT: {:?} => {:?}", msg.path(), arg.changed_properties);
+                true
+            },
+            // _ => {
+            //     true
+            // }, // Do nothing.
+            str => {
+                //println!("Unknown interface_name {:?}", str);
+                true
+            }
+        }
+    }
+
+    conn.add_match(matchadded, interfaces_added_watcher)?;
+    conn.add_match(matchremoved, interfaces_removed_watcher)?;
+    conn.add_match(matchpropertieschanged, properties_changed_watcher)?;
 
     loop {
         let _ret = conn.process(Duration::from_millis(1000))?;//process(Duration::from_millis(5000))?;
